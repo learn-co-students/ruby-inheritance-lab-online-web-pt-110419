996d14fccdc40ca65773f2ab545152657eb3c9b1
Zitai Huang <mywpiedu@gmail.com>
2019-12-19 01:16:53 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,29 @@
-class Student 
-
-end
+class Student < User 
+  attr_accessor :knowledge 
+  def initialize
+    @knowledge = [] 
+  end
+  
+  def learn(str)
+    self.knowledge << str 
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end 
