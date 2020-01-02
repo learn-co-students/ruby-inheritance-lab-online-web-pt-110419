363f3866267f23e0558236bd8e1936307691bfe0
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2020-01-02 14:57:15 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
 class Student 
+  lebron = Student.new
+lebron.first_name = "Lebron"
+james.last_name = "James"
 
+def learn 
+  learn < KNOWLEDGE
 end
+
+lebron.learn(KNOWLEDGE)
---
+++ lib/user.rb
@@ -0,0 +1,3 @@
+class User < Teacher
+  end
+end
