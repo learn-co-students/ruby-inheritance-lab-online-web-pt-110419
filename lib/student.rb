bb5f65eaad30795e7c5f373cd715ebdc4d5f8999
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-16 01:50:53 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require 'pry'
+
+class Student < User
+  
+  def initialize
+    @knowledge = []
+  
+  end
+  
+  
+  def learn(string) 
+    #binding.pry
+    @knowledge << string
+    @knowledge
+  end 
+  
+  def knowledge
+    @knowledge 
+  end 
 
 end
